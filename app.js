dc0d146813a50dbdd8d016a4209a87903376e10f
keonen <keijo.olavi.kangas@gmail.com>
2015-07-28 13:38:35 +0300

    test

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ app.get('/', function (req, res) {
     res.send('Hello World, palvelin on nyt käynnissä ja toimii taas.')
 });
 
+// app.get('/name', function (req, res) {
+    // res.send('Name-hakemiston sisältöä.')
+// });
+
+app.get('/name', function (req, res) {
+    res.send('Testijuttu.')
+});
+
 app.listen(3000);
