39157027213bdff544311fd48c315e804ce6f3ae
keonen <keijo.olavi.kangas@gmail.com>
2015-07-28 14:21:56 +0300

    2015-07-28 14:21

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 var express = require('express');
 var app = express();
 
-// This is called for every request
+// This is called for every request (middleware)
 app.use(function(req, res, next){
-	
+	console.log(req.method);
+	next();
 });
 
-app.get('/', function (req, res) {
-    res.sendfile('index.html')
-});
+app.use('/', express.static('public_html'));
+
+// app.get('/', function (req, res) {
+    // res.sendfile('index.html');
+// });
 
 // app.get('/name', function (req, res) {
     // res.send('Name-hakemiston sisältöä.')
 // });
 
-app.get('/style.css', function (req, res) {
-    res.sendfile('style.css')
-});
+// app.get('/style.css', function (req, res) {
+    // res.sendfile('style.css');
+// });
 
 app.listen(3000);
