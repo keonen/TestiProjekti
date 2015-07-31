d3ad97675bfcb1b596d9996dfcbcd1592723c1fc
keonen <keijo.olavi.kangas@gmail.com>
2015-07-31 11:10:26 +0300

    2015-07-31 11:10

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var db = require('./database');
+var session = require('express-session');
 var app = express();
 
 // This is called for every request (middleware)
 app.use(function(req, res, next){
+    
+    //console.log(res.session.nimi);
 	console.log(req.method);
 	next();
 });
 
+app.use(session({secret: 'testing....'}));
 app.use('/', express.static('public_html'));
 
 //parse application/json
@@ -19,6 +23,8 @@ app.set('views','./views');
 app.set('view engine','jade');
 
 app.post('/contact', function (req, res) {
+    
+    req.session.nimi = req.body.user_name;
 	console.log(req.body);
 	db.addPersonInfo(res, req.body);
 	
@@ -26,8 +32,17 @@ app.post('/contact', function (req, res) {
 });
 
 	app.get('/get_contacts', function (req, res) {
+        
+        if(req.session.nimi === "Keijo")
+        {
 			db.getPersonInfo(res);
-		});
+        }
+        else
+        {
+            res.redirect("/");
+        }
+        
+    });
 
 // app.get('/style.css', function (req, res) {
     // res.sendfile('style.css');
